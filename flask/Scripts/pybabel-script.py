a04cbb7e1fb1daa1e0696989d90eda1309430af5
microflyer <weijiaiosdev@gmail.com>
2013-10-07 23:20:09 +0800

    Remove flask env cause it only works on Windows

--- flask/Scripts/pybabel-script.py
+++
@@ -1,9 +0,0 @@
-#!C:\Users\w.jia\Documents\GitHub\JWSpace\flask\Scripts\python.exe
-# EASY-INSTALL-ENTRY-SCRIPT: 'Babel==1.3','console_scripts','pybabel'
-__requires__ = 'Babel==1.3'
-import sys
-from pkg_resources import load_entry_point
-
-sys.exit(
-   load_entry_point('Babel==1.3', 'console_scripts', 'pybabel')()
-)
